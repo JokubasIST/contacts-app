8219792de314500602bcdc1ede6e057e60f9abc1
JokubasIST <jokubas.pociliauskas@stud.svako.lt>
2025-05-19 11:53:36 +0300

    sutvarkytas email

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\EmailController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -14,9 +15,13 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+
 });
+
 Route::prefix('c')->group(function () {
 Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
 Route::get('/contacts/create', [ContactController::class, 'create'])->middleware('auth')->name('contacts.create');
 Route::post('/contacts', [ContactController::class, 'store'])->middleware('auth')->name('contacts.store');
 });
+
